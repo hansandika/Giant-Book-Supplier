f005fa0fb359c13a53d32f3f5d7bc1549cc01484
hansandika <hansandika70@gmail.com>
2022-11-21 22:22:32 +0700

    add contacts page && modify publisher and layout page

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,99 @@
+<x-app-layout title='Contact Us'>
+    <div class="d-flex justify-content-center gap-5">
+        <div class="d-flex flex-column gap-3">
+            <div class="card">
+                <div class="p-3 d-flex align-items-center gap-5" style="width: 500px">
+                    <h3>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                            class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                            <path
+                                d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                            <path
+                                d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                        </svg>
+                    </h3>
+                    <div>
+                        <div>
+                            <p class="fw-semibold">Chat to Us</p>
+                        </div>
+                        <div>
+                            <p>Our friendly teams is here to help</p>
+                        </div>
+                        <div>
+                            <p class="fw-bold">happybookstore@happy.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="p-3 d-flex align-items-center  gap-5" style="width: 500px">
+                    <h3>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                            class="bi bi-shop" viewBox="0 0 16 16">
+                            <path
+                                d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z" />
+                        </svg>
+                    </h3>
+                    <div>
+                        <div>
+                            <p class="fw-semibold">Office</p>
+                        </div>
+                        <div>
+                            <p>Come to say hello to our office HQ</p>
+                        </div>
+                        <div>
+                            <p class="fw-bold">Jalan Pembangunan baru raya, Kompleks Pertokoan Emerald Blok III / 12
+                            </p>
+                        </div>
+                        <div>
+                            <div class="fw-bold">
+                                Bintaro, Tangerang Selatan, Indonesia
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="p-3 d-flex align-items-center gap-5">
+                    <h3>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                            class="bi bi-telephone-forward" viewBox="0 0 16 16">
+                            <path
+                                d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.762.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                    </h3>
+                    <div>
+                        <div>
+                            <p class="fw-semibold">Phone</p>
+                        </div>
+                        <div>
+                            <p>Mon-Fri from 8am to 8pm.</p>
+                        </div>
+                        <div>
+                            <p class="fw-bold">021-08899776655</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h2>Contact Us</h2>
+            <p>Our friendly team would hear to you</p>
+            <form method="POST">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="First Name" name="firstName">
+                    <input type="text" class="form-control" placeholder="Last Name" name="lastName">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="you@mail.com">
+                </div>
+                <div class="mb-3 form-floating">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="comment" style="height: 200px"></textarea>
+                    <label for="comment">Comments</label>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Send Message</button>
+            </form>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [BookController::class, 'index'])->name('books.index');
 Route::get("/books/categories", [BookController::class, 'index'])->name('books.categories');
 Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
 Route::get("/publishers", [PublisherController::class, 'index'])->name('publishers.index');
 Route::get("/publishers/{publisher}", [PublisherController::class, 'show'])->name('publishers.show');
-Route::view("/contacts", "contact.index")->name('contacts.index');
+Route::get("/contacts", [ContactController::class, 'index'])->name('contacts.index');
